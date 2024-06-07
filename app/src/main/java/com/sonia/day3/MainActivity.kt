d3e24725f150266b71ac6f51a5915a4ffe67c464
devisonia2 <yadavsonia0181@gmail.com>
2024-06-06 16:12:13 -1200

    Initial commit

---
+++ app/src/main/java/com/sonia/day3/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.sonia.day3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    var name: EditText?=null
+    var city: EditText?=null
+    var collegename: EditText?= null
+    var check: Button?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        name=findViewById(R.id.name)
+        city=findViewById(R.id.city)
+        collegename=findViewById(R.id.collegename)
+        check=findViewById(R.id.check)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        check?. setOnClickListener {
+
+            Toast.makeText(this,"Successfully Entered ${name?.text} ${city?.text} ${collegename?.text}", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
